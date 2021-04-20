45b8785b83e273d56c867eede4c93ae38553c23b
AlexDooo <krash_pvp@mail.ru>
2021-04-20 17:19:02 +0300

    Initial commit

---
+++ src/main/java/springboot/resthtpp/ResthtppApplication.java
@@ -0,0 +1,13 @@
+package springboot.resthtpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResthtppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResthtppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springboot/resthtpp/ResthtppApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.resthtpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResthtppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
